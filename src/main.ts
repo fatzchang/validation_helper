c785d39897e867850f798964bcb0e3f25e4d3dd2
fatz <github@fatz.tw>
2020-05-02 09:50:56 +0800

    check email

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-export function twPhone(phone: string): boolean {
+export function checkTwPhone(phone: string): boolean {
   const regex: RegExp = /(^09[0-9]{8}$|^9[0-9]{8}$)/; // allowed (0)9
   return regex.test(phone);;
+}
+
+export const checkEmail = (email: string): boolean => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
 }
